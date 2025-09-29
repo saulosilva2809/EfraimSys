83a677da178319c049b3cf64021fb07fb537ecb4
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-09-29 18:48:32 -0300

    feat: implementa filtros personalizados para transações

--- apps/finances/transactions/views/list_transactions_view.py
+++ apps/finances/transactions/views/list_transactions_view.py
@@ -1,9 +1,13 @@
+from apps.finances.transactions.filters import TransactionFilter
+
 from django.views.generic import ListView
+from django_filters.views import FilterView
 
 from apps.finances.transactions.models import TransactionModel
 
 
-class ListTransactionView(ListView):
+class ListTransactionView(FilterView, ListView):
     model = TransactionModel
     template_name = 'finances/transactions/list_transactions.html'
     paginate_by = 30
+    filterset_class = TransactionFilter
